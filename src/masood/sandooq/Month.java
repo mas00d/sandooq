f089d10ee174b87a1c449e69793d22e548b1aa2e
masood <masood@fake.local>
2022-02-23 14:31:19 +0330

    کلمه آبان در یکی از یادداشت های صورتحساب، اشتباه تایپ شده است. از آنجا که اینترنت بانک سینا، برای ویرایش آن خطا می دهد، مجبور به اضافه کردن املاء اشتباه کلمه آبان به کد شدم

--- src/masood/sandooq/Month.java
+++ src/masood/sandooq/Month.java
@@ -1,44 +1,47 @@
 package masood.sandooq;
 
 public class Month {
-    private static final String[] monthNames = new String[]{
-            "فروردين",
-            "ارديبهشت",
-            "خرداد",
-            "تير",
-            "مرداد",
-            "شهريور",
-            "مهر",
-            "آبان",
-            "آذر",
-            "دي",
-            "بهمن",
-            "اسفند"};
+    private static final String[][] monthNames = new String[][]{
+            {"فروردين"},
+            {"ارديبهشت"},
+            {"خرداد"},
+            {"تير"},
+            {"مرداد"},
+            {"شهريور"},
+            {"مهر"},
+            {"آبان", "آّبان"},
+            {"آذر"},
+            {"دي"},
+            {"بهمن"},
+            {"اسفند"}};
 
     public static int getMonthNumber(String monthName) {
         monthName = monthName.trim();
         for (int i = 0; i < monthNames.length; i++) {
-            if (monthNames[i].equals(monthName)) {
-                return i + 1;
+            for (int j = 0; j < monthNames[i].length; j++) {
+                if (monthNames[i][j].equals(monthName)) {
+                    return i + 1;
+                }
             }
         }
-        //todo: maybe is better throw exception instead of retutn -1.
-        return -1;
+        throw new RuntimeException(monthName + "is invalid");
     }
 
     public static String nextMonth(int monthNumber, int nextCount) {
         //TODO: This try-catch is for test. please delete it as soon as possible
         try {
-            return monthNames[(monthNumber - 1 + nextCount) % 12];
+            return monthNames[(monthNumber - 1 + nextCount) % 12][0];
         } catch (ArrayIndexOutOfBoundsException e) {
             throw new RuntimeException();
         }
     }
 
     public static boolean isValid(String maybeMonthName) {
-        for (String month : monthNames) {
-            if (month.equals(maybeMonthName)) {
-                return true;
+        for (String[] monthName : monthNames) {
+            for (String month : monthName) {
+                if (month.equals(maybeMonthName)) {
+                    return true;
+                }
             }
         }
         return false;
